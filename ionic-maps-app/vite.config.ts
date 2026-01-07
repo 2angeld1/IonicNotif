cbf4d7f08d2b8c33dd6d31c369ac508b0690812d
2angeld19 <angelf@payday.com.pa>
2026-01-07 15:41:18 -0500

    fix: resolve manualChunks typescript error used function approach

--- ionic-maps-app/vite.config.ts
+++ ionic-maps-app/vite.config.ts
@@ -5,4 +5,24 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor';
+            }
+            if (id.includes('@ionic')) {
+              return 'ionic';
+            }
+            if (id.includes('leaflet') || id.includes('react-leaflet')) {
+              return 'leaflet';
+            }
+          }
+        },
+      },
+    },
+  },
 })
